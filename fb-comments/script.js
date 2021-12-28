6fa18b6ff89177b12cd4e4fa0f5062ab6ce1c823
suseendhar lal <lssuseendharlal@gmail.com>
2021-12-28 21:24:51 +0530

    comment-reply

---
+++ fb-comments/script.js
@@ -0,0 +1,47 @@
+const commentObj = [];
+
+const firstBtn = document.getElementById("1-btn");
+const container = document.getElementsByClassName("container")[0];
+
+firstBtn.addEventListener("click", (e) => {
+  generateReplies(e.target.parentElement, e.target.previousElementSibling.value);
+  e.target.previousElementSibling.value = "";
+});
+
+function generateReplies(parent, text, isReplies = false) {
+  let ulEl;
+  if ((!isReplies && parent.children.length < 3) || (isReplies && parent.children.length < 4)) {
+    ulEl = document.createElement("ul");
+    parent.append(ulEl);
+  } else {
+    ulEl = parent.children[2];
+  }
+  const list = document.createElement("li");
+  const span = document.createElement("span");
+  span.innerText = text;
+  const replyBtn = document.createElement("button");
+  replyBtn.textContent = "reply";
+  replyBtn.id = parent.children.length + 1;
+  replyBtn.addEventListener("click", (e, i) => {
+    const div = document.createElement("div");
+    div.classList.add("reply");
+    const replyInput = document.createElement("input");
+    const innerRepBtn = document.createElement("button");
+    innerRepBtn.textContent = "comment";
+    innerRepBtn.id = replyBtn.id + "" + i;
+    innerRepBtn.addEventListener("click", (e) => {
+      generateReplies(e.target.parentElement.parentElement, e.target.previousElementSibling.value, true);
+      div.remove();
+    });
+    const innerCancel = document.createElement("button");
+    innerCancel.textContent = "cancel";
+    innerCancel.addEventListener("click", () => {
+      div.remove();
+    });
+    div.append(replyInput, innerRepBtn, innerCancel);
+    replyBtn.parentElement.append(div);
+    replyInput.focus();
+  });
+  list.append(span, replyBtn);
+  ulEl.append(list);
+}
