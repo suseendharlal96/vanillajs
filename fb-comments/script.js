d983fec918f8720de0ad5593a5cb96b5e05f4765
suseendhar lal <lssuseendharlal@gmail.com>
2021-12-30 19:05:03 +0530

    validation and local storage enabled

--- fb-comments/script.js
+++ fb-comments/script.js
@@ -1,50 +1,83 @@
-const commentObj = [];
+const commentObj = JSON.parse(localStorage.getItem("comments")) || [];
 
-const firstBtn = document.getElementById("1-btn");
+
 const container = document.getElementsByClassName("container")[0];
+const firstBtn = document.getElementById("1-btn");
 
 firstBtn.addEventListener("click", (e) => {
-  generateReplies(e.target.parentElement, e.target.previousElementSibling.value);
-  e.target.previousElementSibling.value = "";
+  if (firstBtn.previousElementSibling.value.trim().length > 0) {
+    let list = createList(commentObj.length + 1, firstBtn.previousElementSibling.value);
+    commentObj.push({ id: commentObj.length + 1, text: firstBtn.previousElementSibling.value, parent: 0 });
+    localStorage.setItem("comments", JSON.stringify(commentObj));
+    firstBtn.previousElementSibling.value = "";
+    appendWithParent(container, list);
+  } else {
+    alert("enter a value");
+  }
 });
 
-function generateReplies(parent, text, isReplies = false) {
-  let ulEl;
-  if ((!isReplies && parent.children.length < 3) || (isReplies && parent.children.length < 4)) {
-    ulEl = document.createElement("ul");
-    parent.append(ulEl);
-  } else {
-    ulEl = parent.children[2];
-  }
+start();
+function start() {
+  commentObj.forEach(({ id, text, parent: parentId }) => {
+    let list = createList(id, text);
+    let parent = container;
+    if (parentId) {
+      parent = document.getElementById(parentId);
+    }
+    appendWithParent(parent, list);
+  });
+}
+
+function createList(id, text) {
   const list = document.createElement("li");
+  list.id = id;
   const span = document.createElement("span");
   span.innerText = text;
   const replyBtn = document.createElement("button");
   replyBtn.textContent = "reply";
-  replyBtn.id = parent.children.length + 1;
-  replyBtn.addEventListener("click", (e, i) => {
-    if (!document.getElementsByClassName("replyInput")[0]) {
-      const div = document.createElement("div");
-      div.classList.add("reply");
-      const replyInput = document.createElement("input");
-      replyInput.classList.add("replyInput");
-      const innerRepBtn = document.createElement("button");
-      innerRepBtn.textContent = "comment";
-      innerRepBtn.id = replyBtn.id + "" + i;
-      innerRepBtn.addEventListener("click", (e) => {
-        generateReplies(e.target.parentElement.parentElement, e.target.previousElementSibling.value, true);
+  replyBtn.addEventListener("click", () => {
+    if (document.getElementsByClassName("reply-container")[0]) return;
+    const div = document.createElement("div");
+    div.classList.add("reply-container");
+    div.classList.add("reply");
+    const replyInput = document.createElement("input");
+    replyInput.classList.add("replyInput");
+    replyInput.placeholder=`reply for ${text}`
+    const innerRepBtn = document.createElement("button");
+    innerRepBtn.textContent = "comment";
+    innerRepBtn.addEventListener("click", () => {
+      const val = replyInput.value;
+      if (val.trim().length > 0) {
         div.remove();
-      });
-      const innerCancel = document.createElement("button");
-      innerCancel.textContent = "cancel";
-      innerCancel.addEventListener("click", () => {
-        div.remove();
-      });
-      div.append(replyInput, innerRepBtn, innerCancel);
-      replyBtn.parentElement.append(div);
-      replyInput.focus();
-    }
+        appendWithParent(list, createList(commentObj.length + 1, val));
+        commentObj.push({ id: commentObj.length + 1, text: replyInput.value, parent: id });
+        localStorage.setItem("comments", JSON.stringify(commentObj));
+      } else {
+        alert("enter a value");
+      }
+    });
+    const innerCancel = document.createElement("button");
+    innerCancel.textContent = "cancel";
+    innerCancel.addEventListener("click", () => {
+      div.remove();
+    });
+    div.append(replyInput, innerRepBtn, innerCancel);
+    list.append(div);
+    replyInput.focus();
   });
   list.append(span, replyBtn);
-  ulEl.append(list);
+  return list;
+}
+
+function appendWithParent(parent, list) {
+  if (parent.childElementCount > 2) {
+    const ul = parent.getElementsByClassName("replyUl")[0];
+    ul.append(list);
+    parent.append(ul);
+  } else {
+    const ulEl = document.createElement("ul");
+    ulEl.classList.add("replyUl");
+    ulEl.append(list);
+    parent.append(ulEl);
+  }
 }
