3e4859136ad2d53e43fba381c308d0e4f4ff5d9d
suseendhar lal <lssuseendharlal@gmail.com>
2022-01-03 20:50:30 +0530

    int observer

--- intersectionObserver/script.js
+++ intersectionObserver/script.js
@@ -1,36 +1,35 @@
-const containerEl = document.querySelector(".container");
-
+const container = document.getElementsByClassName("container")[0];
 const observer = new IntersectionObserver(
   (entries) => {
-    entries.forEach((entry) => {
-      entry.target.classList.toggle("show", entry.isIntersecting);
+    entries.forEach((props) => {
+      console.log(props.target, props.isIntersecting);
+      props.target.classList.toggle("active", props.isIntersecting);
     });
   },
   {
     threshold: 1,
+    // root: container,
   }
 );
 
-createNewCards();
-function createNewCards() {
-  for (let i = 0; i < 10; i++) {
-    const card = document.createElement("div");
-    card.classList.add("card");
-    card.classList.add("show");
-    card.innerText = containerEl.children.length + 1;
-    observer.observe(card);
-    containerEl.appendChild(card);
-  }
+generateList();
+function generateList() {
+  Array.from({ length: 10 }).forEach((_, i) => {
+    const list = document.createElement("div");
+    list.classList.add("list");
+    list.innerText = container.childElementCount + 1;
+    container.append(list);
+    observer.observe(list);
+  });
 }
 
+
 const lastElObserver = new IntersectionObserver((entries) => {
-  const lastEl = entries[0];
-  if (!lastEl.isIntersecting) return;
-  createNewCards();
-  lastElObserver.unobserve(lastEl.target);
-  lastElObserver.observe(document.querySelector(".card:last-child"));
+  if (!entries[0].isIntersecting) return;
+  generateList();
+  lastElObserver.unobserve(entries[0].target);
+  lastElObserver.observe(container.querySelector(".list:last-child"));
 });
 
-const lastCard = document.querySelector(".card:last-child");
+lastElObserver.observe(container.querySelector(".list:last-child"));
 
-lastElObserver.observe(lastCard);
