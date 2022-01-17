d9a2bc24fc13be2c52cfcb0778858c66f86c7c43
suseendhar lal <lssuseendharlal@gmail.com>
2022-01-17 19:20:14 +0530

    prev next month added

--- calender/script.js
+++ calender/script.js
@@ -32,35 +32,32 @@ function createCalender(year, month) {
   calenderEl.innerHTML = "";
   currentMonthEl.innerText = new Date(year, month, 1).toString().split(" ")[1] + " " + year;
   const endDate = new Date(year, month + 1, 0).getDate();
-  const arr = Array.from({ length: 1 }).map(() => new Array(7));
 
   initCalenderEl(0, 1);
 
   const week = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
-  for (let i = 0; i < arr[0].length; i++) {
+  for (let i = 0; i < week.length; i++) {
     const el = document.querySelector(`[data-id="0_${i}"]`);
-    arr[0][i] = week[i];
     el.innerText = week[i];
   }
 
-  let rowNum = arr.length;
+  let rowNum = calenderEl.childElementCount;
   let startDate = 1;
   while (startDate <= endDate) {
-    const colNum = findColumn(new Date(year, month, startDate).toString().split(" ")[0], arr[0]);
-    if (rowNum === arr.length) {
-      initCalenderEl(arr.length, arr.length + 1);
-      arr.push(new Array(7));
+    const colNum = findColumn(new Date(year, month, startDate).toString().split(" ")[0], week);
+    if (rowNum === calenderEl.childElementCount) {
+      initCalenderEl(calenderEl.childElementCount, calenderEl.childElementCount + 1);
     }
     const el = document.querySelector(`[data-id="${rowNum}_${colNum}"]`);
-    arr[rowNum][colNum] = startDate;
-    if (isCurrentDate(startDate, new Date(year, month, 1).toString().split(" ")[1], year)) {
+    if (isToday(startDate, new Date(year, month, 1).toString().split(" ")[1], year)) {
       el.classList.add("current-date");
     }
     el.innerText = startDate;
     if (colNum === 6) rowNum++;
     startDate++;
   }
+  populatePrevNextMonth();
 
   function initCalenderEl(start, end) {
     for (let i = start; i < end; i++) {
@@ -76,19 +73,54 @@ function createCalender(year, month) {
     }
   }
 
-  function findColumn(weekName, firstRow) {
-    for (let i = 0; i < firstRow.length; i++) {
-      if (firstRow[i] === weekName) {
+  function findColumn(weekName, week) {
+    for (let i = 0; i < week.length; i++) {
+      if (week[i] === weekName) {
         return i;
       }
     }
   }
 
-  function isCurrentDate(date, month, year) {
+  function isToday(date, month, year) {
     const today = new Date();
     const todayDate = today.toString().split(" ")[2];
     const todayMonth = today.toString().split(" ")[1];
     const todayYear = today.toString().split(" ")[3];
     return todayDate === date.toString() && todayMonth === month && todayYear === year.toString();
+  }
+
+  function populatePrevNextMonth() {
+    let prevMonth = new Date(year, month, 0).getDate();
+    for (let i = 6; i >= 0; i--) {
+      if (!calenderEl.children[1].children[i].innerText) {
+        const col = calenderEl.children[1].children[i];
+        col.innerText = prevMonth--;
+        col.classList.add("blur");
+        col.addEventListener("click", () => {
+          currMonth -= 1;
+          if (currMonth < 0) {
+            currMonth = 11;
+            currYear -= 1;
+          }
+          createCalender(currYear, currMonth);
+        });
+      }
+    }
+    let nextMonth = 1;
+    for (let i = 0; i < 7; i++) {
+      if (!calenderEl.children[calenderEl.children.length - 1].children[i].innerText) {
+        const col = calenderEl.children[calenderEl.children.length - 1].children[i];
+        col.innerText = nextMonth++;
+        col.classList.add("blur");
+        col.addEventListener("click", () => {
+          currMonth++;
+          if (currMonth === 12) {
+            currYear += 1;
+          }
+          currMonth = currMonth % 12;
+          createCalender(currYear, currMonth);
+        });
+      }
+    }
   }
 }
